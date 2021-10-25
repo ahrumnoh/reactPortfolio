861c5f0670b8d6069093e44117c2ae0d541d87a7
ahrum noh <sydneyroseenglish@gmail.com>
2021-10-25 16:46:45 +1100

    amended
    changes

--- ahrumnoh/src/serviceWorker.js
+++ ahrumnoh/src/serviceWorker.js
@@ -21,17 +21,15 @@ export function register(config) {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
-        // This is running on localhost. Let's check if a service worker still exists or not.
+    
         checkValidServiceWorker(swUrl, config);
 
-        // Add some additional logging to localhost, pointing developers to the
-        // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' );
         });
       } else {
-        // Is not localhost. Just register service worker
+   
         registerValidSW(swUrl, config);
       }
     });
@@ -50,9 +48,7 @@ function registerValidSW(swUrl, config) {
         installingWorker.onstatechange = () => {
           if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
-              // At this point, the updated precached content has been fetched,
-              // but the previous service worker will still serve the older
-              // content until all client tabs are closed.
+     
               console.log(
                 'New content is available and will be used when all ' +
                   'tabs for this page are closed. '
@@ -63,9 +59,7 @@ function registerValidSW(swUrl, config) {
                 config.onUpdate(registration);
               }
             } else {
-              // At this point, everything has been precached.
-              // It's the perfect time to display a
-              // "Content is cached for offline use." message.
+
               console.log('Content is cached for offline use.');
 
               // Execute callback
@@ -83,10 +77,10 @@ function registerValidSW(swUrl, config) {
 }
 
 function checkValidServiceWorker(swUrl, config) {
-  // Check if the service worker can be found. If it can't reload the page.
+
   fetch(swUrl)
     .then(response => {
-      // Ensure service worker exists, and that we really are getting a JS file.
+  
       const contentType = response.headers.get('content-type');
       if (
         response.status === 404 ||
